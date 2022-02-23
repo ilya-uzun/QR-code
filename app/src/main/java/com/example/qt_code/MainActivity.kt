71c4ad33ee5148a5fe1a64b7481de5384472bb0c
ilya-uzun <ilya.uzun@bk.ru>
2022-02-23 18:47:27 +0500

    кнопка

--- app/src/main/java/com/example/qt_code/MainActivity.kt
+++ app/src/main/java/com/example/qt_code/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.qt_code
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            textView.text = "Hello"
+        }
     }
 }
